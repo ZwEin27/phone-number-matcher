c7fdcce07e5ca61de85d310b2ba189f19455b5ab
ZwEin <zwein27@gmail.com>
2016-06-09 13:39:27 -0700

    import en library

--- pnmatcher/__init__.py
+++ pnmatcher/__init__.py
@@ -2,7 +2,18 @@
 
 """
 @auther: Lingzhe Teng
-@version: alpha
+@version: in coding
 
 """
 
+import sys
+import os
+sys.path.append(os.path.join(os.path.abspath('.'), 'vendor'))
+import en
+
+class PhoneNumberMatcher():
+    def __init__(self):
+        pass
+
+
+print en.spelling.correct('2one9')
