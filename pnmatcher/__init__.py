af4f82a9a87756c0e9ecd16d38e6837a7ad056b7
ZwEin <zwein27@gmail.com>
2016-06-09 13:44:13 -0700

    initial core

--- pnmatcher/__init__.py
+++ pnmatcher/__init__.py
@@ -8,12 +8,12 @@
 
 import sys
 import os
+
 sys.path.append(os.path.join(os.path.abspath('.'), 'vendor'))
-import en
+# import en
+# print en.spelling.correct('2one9')
 
 class PhoneNumberMatcher():
     def __init__(self):
         pass
 
-
-print en.spelling.correct('2one9')
