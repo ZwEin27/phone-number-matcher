eeba36bac76c2e5152213c6c11370f85df2d2eed
ZwEin <zwein27@gmail.com>
2016-06-11 23:36:25 -0700

    update preprocessor for faerie

--- pnmatcher/core/preprocessor/__init__.py
+++ pnmatcher/core/preprocessor/__init__.py
@@ -7,25 +7,37 @@ Available 24/7 *** 214 784 2976 ***
 """
 import faerie
 import os
+import json
 
-TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'faerie_conf')
+FAERIE_CONF_DIR = os.path.join(os.path.dirname(__file__), 'faerie_conf')
 
-dictionary_ = os.path.expanduser(os.path.join(TEST_DATA_DIR, 'numbers_dictionary.json'))
-documents_ = os.path.expanduser(os.path.join(TEST_DATA_DIR, 'numbers_documents.json'))
-config_ = os.path.expanduser(os.path.join(TEST_DATA_DIR, 'numbers_config.json'))
+dictionary_ = os.path.expanduser(os.path.join(FAERIE_CONF_DIR, 'numbers_dictionary.json'))
+# documents_ = os.path.expanduser(os.path.join(TEST_DATA_DIR, 'numbers_documents.json'))
+config_ = os.path.expanduser(os.path.join(FAERIE_CONF_DIR, 'numbers_config.json'))
 
+faerie_config = json.loads(open(config_).read())
+faerie_dictionary = faerie.readDict(dictionary_, faerie_config)
 
 class Preprocessor():
 
     def __init__(self):
         pass
 
+    def replace_notable_numbers(self, raw):
+        jsonline = {'number': raw, 'id': 'single'}
+        # jsonline = json.dumps(raw)
+        jsonline = faerie.processDoc(jsonline, faerie_dictionary, faerie_config)
+
+
+
+        return jsonline
+
     def clean(self, raw):
-        raw = faerie.run(dictionary_,documents_,config_)
+        raw = self.replace_notable_numbers(raw)
 
         return raw
 
     def preprocess(self, raw):
         raw = self.clean(raw)
-
+        return raw
 
