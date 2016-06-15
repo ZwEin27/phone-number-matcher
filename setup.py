60efb55ca1c0955b08d46313c2254c650e436aa7
ZwEin <zwein27@gmail.com>
2016-06-14 19:09:44 -0700

    initial setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import Extension,find_packages
+from os import path
+
+setup(
+    name = 'phone-number-matcher',
+    version = '0.0.1',
+    description = 'match phone numbers in url and text',
+    author = 'Lingzhe Teng',
+    author_email = 'zwein27@gmail.com',
+    url = 'https://github.com/ZwEin27/phone-number-matcher',
+    packages = find_packages(),
+    keywords = ['phone_number', 'extractor'],
+    install_requires=['phonenumbers']
+)
